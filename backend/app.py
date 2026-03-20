5e7e86aaec1206667c14c494d96e86e67b3192e8
bhaktiyadav08 <bhaktiyadav795@gmail.com>
2026-03-20 22:08:52 +0530

    added flask backend foolder skeleton

---
+++ backend/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from ml.predict import predict_status
+from pymongo import MongoClient
+from datetime import datetime
+
+app = Flask(__name__)
+CORS(app)
+# MongoDB connection
+client = MongoClient("mongodb://localhost:27017/")
+db = client["dialysense"]
+collection = db["sensor_data"]
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+
+    temperature = float(data['temperature'])
+    flow_rate = float(data['flow_rate'])
+    water_level = float(data['water_level'])
+
+    result = predict_status(temperature, flow_rate, water_level)
+
+    # Save to database
+    record = {
+        "temperature": temperature,
+        "flow_rate": flow_rate,
+        "water_level": water_level,
+        "status": result,
+        "timestamp": datetime.now()
+    }
+
+    collection.insert_one(record)
+
+    return jsonify({"status": result})
+@app.route('/data', methods=['GET'])
+def get_data():
+    records = list(collection.find().sort("timestamp", -1).limit(20))
+
+    for r in records:
+        r['_id'] = str(r['_id'])
+
+    return jsonify(records)
+
+if __name__ == '__main__':
+    app.run(debug=True)
